edb7abd26b29dd9ade39ee13748ef6c8598f0731
MiyueSC <913784771@qq.com>
2024-05-06 16:21:56 +0800

    feat: add examples

---
+++ examples/app/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import { CREATE_OPTIONS } from '@miyue-bpmn/modules/palette/PaletteProvider'
+
+  console.log(CREATE_OPTIONS)
+</script>
+
+<template>
+  <div id="pages-app"> </div>
+</template>
+
+<style>
+  #pages-app {
+    height: 100%;
+    font-family: 'Hannotate SC';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
--- packages/modules/index.ts
+++ packages/modules/index.ts
@@ -1,3 +1,5 @@
+import type { ModuleDeclaration } from 'bpmn-js/lib/BaseViewer'
+
 import GridModule from 'diagram-js-grid-bg'
 
 import CustomCmd from './cmd'
@@ -5,8 +7,27 @@ import CustomRules from './rules'
 import Translate from './translate'
 import BpmnLintModule from './lint'
 
-const modules = [CustomCmd, CustomRules, Translate, GridModule, BpmnLintModule]
+const allModules = [CustomCmd, CustomRules, Translate, GridModule, BpmnLintModule]
+
+const allModulesMap = {
+  multiCommands: CustomCmd,
+  rules: CustomRules,
+  translate: Translate,
+  gridBackground: GridModule,
+  bpmnLint: BpmnLintModule
+} as const
+
+export type ModuleName = keyof typeof allModulesMap
 
 export { lintConfig } from './lint'
 
-export default modules
+export const getAdditionalModules = (moduleNames?: ModuleName[]) => {
+  if (!moduleNames?.length) return allModulesMap
+  const modules: ModuleDeclaration[] = []
+  for (const moduleName of moduleNames) {
+    allModulesMap[moduleName] && modules.push(allModulesMap[moduleName])
+  }
+  return modules
+}
+
+export default allModules
