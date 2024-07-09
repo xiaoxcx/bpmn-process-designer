1ef3d8281d03bb4b3155ef92db2291844a96f826
miyuesc <913784771@qq.com>
2024-07-09 14:36:55 +0800

    feat: update i18n resource

--- packages/modules/translate/resources/index.ts
+++ packages/modules/translate/resources/index.ts
@@ -1,15 +1,55 @@
 const externalElements = {
-  'Copy Service Task': '抄送任务',
-  'Camel Service Task': '骆驼任务',
-  'Sc Service Task': '微服务任务',
-  'Rest Service Task': 'Rest 任务',
-  'Dmn Service Task': '决策任务',
-  'Http Service Task': 'Http 任务',
-  'Mq Service Task': '消息任务',
-  'Mail Service Task': '邮件任务',
   'Call activity': '调用活动',
 
-  'Terminate end event': '终止结束事件'
+  'Sub-process': '子流程',
+  'Event sub-process': '事件子流程',
+  'Sub-process (collapsed)': '子流程(折叠)',
+  'Sub-process (expanded)': '子流程(展开)',
+  'Ad-hoc': '临时子流程',
+  'Create expanded sub-process': '创建子流程(折叠)',
+
+  'Variable start event': '参数开始事件',
+  'Variable start event (non-interrupting)': '参数开始事件(非中断)',
+  'Variable boundary event': '参数边界事件',
+  'Variable boundary event (non-interrupting)': '参数边界事件(非中断)',
+  'Variable intermediate catch event': '中间参数捕获事件',
+  'Terminate end event': '终止结束事件',
+  Transaction: '事务子流程',
+  'Boundary event': '边界事件',
+  'Expanded pool/participant': '泳道（展开）',
+
+  'Create element': '创建元素',
+  'Append element': '追加元素',
+  Gateways: '网关',
+  Tasks: '任务',
+  'Sub-processes': '子流程',
+  Events: '事件',
+  Data: '数据对象',
+  Participants: '协助',
+
+  // bpmnlint
+  '{errors} Errors, {warnings} Warnings': '{errors} 错误, {warnings} 警告',
+  'No Issues': '没问题',
+
+  // token simulation
+  'Simulation Log': '模拟日志',
+  'No Entries': '暂无数据',
+  Started: '已启动',
+  Finished: '已结束',
+  'Process started': '流程启动',
+  'SubProcess started': '子流程启动',
+  'Process finished': '流程结束',
+  'SubProcess finished': '子流程结束',
+  'Process canceled': '流程取消',
+  'SubProcess canceled': '子流程取消',
+  'Intermediate event': '中间事件',
+  'Reset Simulation': '重置状态',
+  'Play/Pause Simulation': '继续/暂停',
+  'Play Simulation': '开始模拟',
+  'Pause Simulation': '暂停模拟',
+  'Toggle Simulation Log': '模拟日志',
+  'Show Simulation Log': '显示日志',
+  'Hide Simulation Log': '隐藏日志'
 }
 
 export default externalElements
