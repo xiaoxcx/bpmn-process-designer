381ff42999ec08f83ce068a2365dea1c1208869b
MiyueFE <913784771@qq.com>
2025-01-20 14:39:38 +0800

    Update index.ts

--- packages/designer/index.ts
+++ packages/designer/index.ts
@@ -1,5 +1,8 @@
 import Viewer, { type ViewerOptions } from '@miyue-bpmn/viewer'
 import Modeler from 'bpmn-js/lib/Modeler'
+import { isUndefined } from 'min-dash'
+import { DEFAULT_DISTANCE } from 'diagram-js/lib/features/auto-place/AutoPlaceUtil'
+import Palette from 'diagram-js/lib/features/palette/Palette'
 
 export default class Designer extends Modeler {
   getCanvas: typeof Viewer.prototype.getCanvas
@@ -9,6 +12,9 @@ export default class Designer extends Modeler {
   autoZoomAndCenter: typeof Viewer.prototype.autoZoomAndCenter
   autoElementCenter: typeof Viewer.prototype.autoElementCenter
   toggleTheme: typeof Viewer.prototype.toggleTheme
+
+  private _insertElement: (targetShape: BpmnElement, newElement: string, distance?: number) => void
+  updatePalette: () => void
 
   constructor(options: ViewerOptions) {
     super(options)
@@ -20,5 +26,19 @@ export default class Designer extends Modeler {
     this.autoZoomAndCenter = Viewer.prototype.autoZoomAndCenter.bind(this)
     this.autoElementCenter = Viewer.prototype.autoElementCenter.bind(this)
     this.toggleTheme = Viewer.prototype.toggleTheme.bind(this)
+
+    // 刷新 palette 内容
+    this.updatePalette = function () {
+      this.get<Palette & { _rebuild: () => void }>('palette')._rebuild()
+    }
+
+    this._insertElement = function (targetShape, newElement, distance) {
+      if (!targetShape) {
+        return console.error('Target shape not found')
+      }
+      if (isUndefined(distance)) {
+        distance = DEFAULT_DISTANCE
+      }
+    }
   }
 }
